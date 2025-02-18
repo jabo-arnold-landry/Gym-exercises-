6aa5eed57270eef5483e981dd09494e642884dc7
jabo arnold landry <arnoldjab@gmail.com>
2025-02-18 18:50:27 +0200

    18/2/2025

--- exercise2.js
+++ exercise2.js
@@ -20,3 +20,24 @@ function rainDrops(drop) {
   }
 }
 console.log(rainDrops(15));
+
+function transform(value) {
+  const valueString = value.toString();
+  const numDigits = valueString.length;
+  let sum = 0;
+  for (let i = 0; i <= numDigits; i++) {
+    sum += Math.pow(parseInt(valueString[i]), numDigits);
+  }
+  return sum === value;
+}
+console.log(transform(7));
+// sorting out letters
+
+function longest(s1, s2) {
+  // your code
+  const combined = s1 + s2;
+  const uniqueChars = new Set(combined);
+  const arr = Array.from(uniqueChars);
+  return arr.sort().join("");
+}
+console.log(longest("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
