56bb707c523d9d10d6f00c4e6f81c94f03a4fdcd
jabo arnold landry <arnoldjab@gmail.com>
2025-07-19 13:27:07 +0200

    intro-to-traversal-tree

--- graphAlg.js
+++ graphAlg.js
@@ -10,12 +10,38 @@ function depthFirstLoop(graph, node) {
     }
   }
 }
+
+function depthFirstRecursive(graph, node) {
+  console.log(node);
+  for (let neighbor of graph[node]) {
+    depthFirstRecursive(graph, neighbor);
+  }
+}
+function firstDepthRecursive(graph, node) {
+  console.log(node);
+  for (let key of graph[node]) {
+    firstDepthRecursive(graph, key);
+  }
+}
 const graphs = {
-  a: ["c", "b"],
+  a: ["b", "c"],
   b: ["d"],
   c: ["e"],
   d: [],
   e: ["f"],
   f: [],
 };
-depthFirstLoop(graphs, "a");
+
+function firstDepth(graph, node) {
+  const queue = [node];
+  while (queue.length > 0) {
+    const currElement = queue.shift();
+    console.log(currElement);
+    for (let neighbor of graph[currElement]) {
+      queue.push(neighbor);
+    }
+  }
+}
+firstDepth(graphs, "a");
+
+depthFirstRecursive(graphs, "a");
