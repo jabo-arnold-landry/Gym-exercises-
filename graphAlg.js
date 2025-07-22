141684d3f3aef35a71f856c75e92f619856b5b21
jabo arnold landry <arnoldjab@gmail.com>
2025-07-22 11:24:34 +0200

    undirected-graph

--- graphAlg.js
+++ graphAlg.js
@@ -1,47 +1,75 @@
-//this file walks me through how graph algorithms works in combination with firtdepth and breadth first traversal algorithms works
+// //this file walks me through how graph algorithms works in combination with firtdepth and breadth first traversal algorithms works
 
-function depthFirstLoop(graph, node) {
-  let arr = [node];
-  while (arr.length > 0) {
-    let currentElement = arr.pop();
-    console.log(currentElement);
-    for (let neighbor of graph[currentElement]) {
-      arr.push(neighbor);
-    }
-  }
-}
+// function depthFirstLoop(graph, node) {
+//   let arr = [node];
+//   while (arr.length > 0) {
+//     let currentElement = arr.pop();
+//     console.log(currentElement);
+//     for (let neighbor of graph[currentElement]) {
+//       arr.push(neighbor);
+//     }
+//   }
+// }
 
-function depthFirstRecursive(graph, node) {
-  console.log(node);
-  for (let neighbor of graph[node]) {
-    depthFirstRecursive(graph, neighbor);
-  }
-}
-function firstDepthRecursive(graph, node) {
-  console.log(node);
-  for (let key of graph[node]) {
-    firstDepthRecursive(graph, key);
-  }
-}
+// function depthFirstRecursive(graph, node) {
+//   console.log(node);
+//   for (let neighbor of graph[node]) {
+//     depthFirstRecursive(graph, neighbor);
+//   }
+// }
+// function firstDepthRecursive(graph, node) {
+//   console.log(node);
+//   for (let key of graph[node]) {
+//     firstDepthRecursive(graph, key);
+//   }
+// }
 const graphs = {
-  a: ["b", "c"],
-  b: ["d"],
-  c: ["e"],
-  d: [],
-  e: ["f"],
-  f: [],
+  f: ["g", "i"],
+  g: ["h"],
+  h: [],
+  i: ["g", "k"],
+  j: ["i"],
+  k: [],
 };
 
-function firstDepth(graph, node) {
-  const queue = [node];
+// function firstDepth(graph, node) {
+//   const queue = [node];
+//   while (queue.length > 0) {
+//     const currElement = queue.shift();
+//     console.log(currElement);
+//     for (let neighbor of graph[currElement]) {
+//       queue.push(neighbor);
+//     }
+//   }
+// }
+// firstDepth(graphs, "a");
+
+// depthFirstRecursive(graphs, "a");
+
+function checkingPath(graph, src, dest) {
+  const queue = [src];
   while (queue.length > 0) {
-    const currElement = queue.shift();
-    console.log(currElement);
-    for (let neighbor of graph[currElement]) {
+    const curr = queue.shift();
+    for (let neighbor of graph[curr]) {
+      if (neighbor === dest) return true;
       queue.push(neighbor);
     }
   }
+  return false;
 }
-firstDepth(graphs, "a");
+console.log(checkingPath(graphs, "f", "k"));
 
-depthFirstRecursive(graphs, "a");
+// undirected graph
+const undirectedGraph = (edges, nodeA, nodeB) => {
+  helperfn(edges);
+};
+const helperfn = (edges) => {
+  let graph = {};
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!a in graph) graph[a] = [];
+    if (!b in graph) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+};
