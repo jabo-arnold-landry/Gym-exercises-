4c1770c0a7f3808d7810d05a28fd4f84abd1b165
jabo arnold landry <arnoldjab@gmail.com>
2025-08-05 12:20:05 +0200

    snail-kata-final

--- snail.js
+++ snail.js
@@ -1,21 +1,30 @@
-let snail = function (array) {
-  let results = [];
-  let counter = 1;
-  do {
-    results.push(...array[0].slice(0, 3));
-  } while (counter < results);
-  {
-    for (let i = 0; i < array[counter].reverse().length; i++) {
-      results.push(array[counter][i]);
-    }
-    counter += 1;
-  }
-  return results;
-};
+function snail(array) {
+  if (array.length === 0 || array[0].length === 0) return [];
+
+  // Top row
+  const top = array[0];
+  // Right column
+  const right = array.slice(1, -1).map((row) => row.pop());
+  // Bottom row (reversed)
+  const bottom = array.length > 1 ? array[array.length - 1].reverse() : [];
+
+  // Left column (bottom to top)
+  const left = array
+    .slice(1, -1)
+    .reverse()
+    .map((row) => row.shift());
+
+  // Middle matrix
+  const middle = array.slice(1, -1).map((row) => row.slice(0));
+
+  // Recursively process the middle
+  return [...top, ...right, ...bottom, ...left, ...snail(middle)];
+}
+
 console.log(
   snail([
     [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+    [8, 9, 4],
+    [7, 6, 5],
   ])
 );
