3dfdfc833adb4493b071a9f3e11c2ba36b160d8f
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-02-03 17:28:21 +0200

    feat:Solution for converting a text to a number kata using TypeScript

---
+++ TypeScriptSolutions/StringToNumber.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function stringToNumber(str) {
+    return Number(str);
+}
+console.log(stringToNumber("4"));
+//# sourceMappingURL=StringToNumber.js.map
