c1409980c033e9e294e51a35030ecf0a658a8d6f
Jabo Arnold Landry <arnoldjabo@gmail.com>
2026-03-02 13:45:16 +0200

    feat: add alphanumeric function to validate string characters

---
+++ TypeScriptKatas/Notverysecure.ts
@@ -0,0 +1,11 @@
+function alphanumeric(string: string): boolean {
+  if (!string.trim().length) return false;
+  const regex = /[_]|\W\s/gim;
+  if (!regex.test(string.trim())) return true;
+  return false;
+}
+
+console.log(alphanumeric("Mazinkaiser")); // true
+console.log(alphanumeric("hello world_")); //false
+console.log(alphanumeric("PassW0rd")); //true
+console.log(alphanumeric("     ")); //false
