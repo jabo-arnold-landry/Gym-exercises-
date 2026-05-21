9deb720ceb1fe71381bc5dee1f5333ed93984990
jabo <arnoldjabo@gmail.com>
2026-05-21 15:39:13 +0200

    add the equals method on the object prototype to be accessed by every object

--- TypeScriptKatas/vector.ts
+++ TypeScriptKatas/vector.ts
@@ -1,4 +1,5 @@
 type ArrOrObjType = number[] | Vector;
+
 class Vector {
   public components: number[];
   constructor(components: number[]) {
@@ -12,8 +13,7 @@ class Vector {
       throw Error("length of arrays must be equal!");
     }
 
-    this.components = arr.map((num, index) => num + this.components[index]!);
-    return this;
+    return arr.map((num, index) => num + this.components[index]!);
   }
 
   subtract(arr: ArrOrObjType) {
@@ -22,8 +22,7 @@ class Vector {
     if (arr.length !== this.components.length)
       throw Error("length of arrays must be equal!");
 
-    this.components = arr.map((num, index) => num - this.components[index]!);
-    return this;
+    return arr.map((num, index) => num - this.components[index]!);
   }
 
   dot(arr: ArrOrObjType) {
@@ -42,28 +41,44 @@ class Vector {
       .reduce((sum, num) => (sum += num));
     return Math.sqrt(result);
   }
-
-  equals(arr: ArrOrObjType) {
-    if (!Array.isArray(arr) && typeof arr === "object") arr = arr["components"];
-
-    if (arr.length !== this.components.length) return false;
-    const booleanValues = arr.map((num, index) => {
-      if (num !== this.components[index]!) return false;
-    });
-    return booleanValues.includes(false) ? false : true;
-  }
-
-  valueOf() {
-    return this.components;
-  }
-
-
 }
+declare global {
+  interface Object {
+    equals(obj: any): boolean;
+  }
+}
+
+Object.prototype.equals = function (obj: any): boolean {
+  // Case 1: Vector comparison
+  if (this instanceof Vector && obj instanceof Vector) {
+    return (
+      this.components.length === obj.components.length &&
+      this.components.every((el, i) => el === obj.components[i])
+    );
+  }
+
+  // Case 2: Array comparison
+  if (Array.isArray(this) && Array.isArray(obj)) {
+    return (
+      this.length === obj.length &&
+      this.every((el: any, i: number) => el === obj[i])
+    );
+  }
+
+  // Case 3: Plain object comparison
+  if (typeof this === "object" && typeof obj === "object") {
+    const keysA = Object.keys(this);
+    const keysB = Object.keys(obj);
+    if (keysA.length !== keysB.length) return false;
+    return keysA.every((k) => (this as any)[k] === obj[k]);
+  }
+
+  // Case 4: Primitive fallback
+  return this === obj;
+};
 
 const a = new Vector([1, 2, 3]);
 const b = new Vector([3, 4, 5]);
 const c = new Vector([5, 6, 7, 8]);
 
-console.log(a.add(b));
-// console.log(a.dot(b));
-// console.log(a.equals(b));
+console.log(a.equals(new Vector([4, 6, 80])));
